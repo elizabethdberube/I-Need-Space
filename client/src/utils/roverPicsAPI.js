fdc0adc4ea7e21382cf7cb2b1af91a6ac63a447f
Elizabeth Berube <elizabethdberube@gmail.com>
2022-05-23 21:05:13 -0400

    Update roverPicsAPI.js

--- client/src/utils/roverPicsAPI.js
+++ client/src/utils/roverPicsAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const apiKey = "Zo8kqmCDwTNnP4Z5aMz5uyoVq6wR2BFGTT9tqNGe"
+
 const roverPicsSearch = async (query) =>
 
     axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&camera=fhaz&api_key=${apiKey}`);
