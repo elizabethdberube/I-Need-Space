877db251210c1d8dc045413dc2db0ac16945479b
MichaelR <russiffilli@gmail.com>
2022-06-06 22:27:58 -0400

    commit

--- client/src/components/Rover.js
+++ client/src/components/Rover.js
@@ -137,66 +137,7 @@ export default function RoverPage() {
                   </li>
                 ))}
               </ul>
-=======
-    const { loading: loading_FHAZ, data: data_FHAZ } = useQuery(FHAZ_Data);
-    const { loading: loading_RHAZ, data: data_RHAZ } = useQuery(RHAZ_Data);
-    const { loading: loading_NAVCAM, data: data_NAVCAM } = useQuery(NAVCAM_Data);
-    const { loading: loading_twitter, data: data_twitter } = useQuery(twitter_Data);
 
-
-    const [rover, setRover] = useState("");
-
-
-    const twitter = data_twitter?.getTwitter || [];
-
-    const RHAZ = data_RHAZ?.getRHAZ || {};
-
-    const FHAZ = data_FHAZ?.getFHAZ || {};
-
-    const NAVCAM = data_NAVCAM?.getNAVCAM || {};
-
-    //load rover pic
-    useEffect(() => {
-        setRover(roverPic);
-    }, []);
-
-    //change rover pic
-    const setRHAZ = () => {
-
-        setRover(RHAZ.img_src);
-
-    };
-
-
-    //change rover pic
-    const setFHAZ = () => {
-        setRover(FHAZ.img_src);
-
-    };
-
-
-    //change rover pic
-    const setNAVCAM = () => {
-        setRover(NAVCAM.img_src);
-
-
-    };
-    if (loading_FHAZ || loading_RHAZ || loading_NAVCAM || loading_twitter) {
-        return <div>Loading...</div>;
-    } return (
-        <>
-            <div className="wrapper">
-
-
-                <img src={rover}></img>
-                <div src={FHAZ.name}></div>
-                <div src={FHAZ.status}></div>
-                <div src={FHAZ.launch_date}></div>
-                <div src={FHAZ.landing_date}></div>
-                <div>Click the different buttons to see Curiosity's different camera angles</div>
-                <button onClick={setFHAZ}>Front Hazard Avoidance Camera</button>
-                <button onClick={setRHAZ}>Rear Hazard Avoidance Camera </button>
-                <button onClick={setNAVCAM}>Navigation Camera</button>
 
             </div>
           </Card >
@@ -205,6 +146,6 @@ export default function RoverPage() {
       </div>
     </>
   );
-}
+};
 
 
